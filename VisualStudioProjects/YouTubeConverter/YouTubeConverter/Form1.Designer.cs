22cb24d0ed9572d060b2ad2df21b13902a8c395c
James Chandler <jameschandlerjr@hotmail.com>
2018-04-01 01:15:44 -0400

    Added Youtube Converter Project

---
+++ VisualStudioProjects/YouTubeConverter/YouTubeConverter/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace YouTubeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderPath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelCheck = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(357, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter URL Path here";
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(446, 79);
+            this.checkButton.Margin = new System.Windows.Forms.Padding(4);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(133, 28);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "Check URL";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 113);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(226, 182);
+            this.axShockwaveFlash1.Margin = new System.Windows.Forms.Padding(4);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(342, 240);
+            this.axShockwaveFlash1.TabIndex = 3;
+            this.axShockwaveFlash1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 161);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "While you wait, enjoy your video:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Click the Path button to save your file(s).";
+            // 
+            // folderPath
+            // 
+            this.folderPath.Location = new System.Drawing.Point(484, 41);
+            this.folderPath.Margin = new System.Windows.Forms.Padding(4);
+            this.folderPath.Name = "folderPath";
+            this.folderPath.Size = new System.Drawing.Size(95, 28);
+            this.folderPath.TabIndex = 6;
+            this.folderPath.Text = "Path";
+            this.folderPath.UseVisualStyleBackColor = true;
+            this.folderPath.Click += new System.EventHandler(this.folderPath_Click);
+            // 
+            // labelCheck
+            // 
+            this.labelCheck.AutoSize = true;
+            this.labelCheck.Location = new System.Drawing.Point(222, 85);
+            this.labelCheck.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCheck.Name = "labelCheck";
+            this.labelCheck.Size = new System.Drawing.Size(0, 17);
+            this.labelCheck.TabIndex = 7;
+            this.labelCheck.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 439);
+            this.Controls.Add(this.labelCheck);
+            this.Controls.Add(this.folderPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Youtube Video Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button button2;
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button folderPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label labelCheck;
+    }
+}
+
---
+++ VisualStudioProjects/YouTubeConverter/YouTubeConverter/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace YouTubeConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string url = textBox1.Text;
+            // Put into method later
+            /* var youtube = YouTube.Default;
+            var video = youtube.GetVideo(); */
+            if (url.Contains("youtube.com/watch?v=") || url.Contains("youtu.be/"))
+            {
+                labelCheck.ForeColor = Color.Green;
+                labelCheck.MaximumSize = new Size(200, 0);
+                labelCheck.Text = "Link works! Proceed to download!";
+                labelCheck.Visible = true;
+                button2.Visible = true;
+            }
+            else
+            {
+                labelCheck.ForeColor = Color.Red;
+                labelCheck.MaximumSize = new Size(100, 0);
+                labelCheck.AutoSize = true;
+                labelCheck.Text = "Link does not work! Please try again!";
+                labelCheck.Visible = true;
+                button2.Visible = false;
+            }
+
+        }
+
+        private void folderPath_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.label2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string url = textBox1.Text;
+            axShockwaveFlash1.Visible = true;
+            label1.Visible = true;
+            // https://youtu.be/62ZidB9RgXs dis a good song doe
+            if (url.Contains("watch?v="))
+            {
+                string youtubeUrl = url.Replace("watch?v=", "v/");
+                axShockwaveFlash1.Movie = youtubeUrl;
+                axShockwaveFlash1.Play();
+            }
+
+            if (url.Contains("youtu.be/"))
+            {
+                string youtubeUrl = url.Replace("youtu.be/", "www.youtube.com/v/");
+                axShockwaveFlash1.Movie = youtubeUrl;
+                axShockwaveFlash1.Play();
+            }
+        }
+    }
+}
