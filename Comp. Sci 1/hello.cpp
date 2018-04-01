6c3ae9b1d57ca94b076aa15d06b8fcc0091a2639
James Chandler <jameschandlerjr@hotmail.com>
2018-04-01 00:15:24 -0400

    Add files via upload
    Added loose (trivial) file.

---
+++ Comp. Sci 1/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+string me;
+
+int main()
+
+{
+	cout <<"What is your name?" <<endl;
+	cin >> me;
+	cout << endl;
+	cout <<" Hello "<< me << endl;
+	return 0;
+
+}
